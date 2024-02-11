fe49e0d9214456bf588354c63452f73748f1d4f5
Kaleb Soller <kalebsoller@gmail.com>
2024-02-11 12:16:29 -0500

    CH 7. Error & Mitigation.

--- CH 7. Loops.py
+++ CH 7. Loops.py
@@ -2,3 +2,8 @@
 
 # LESSON 1
 print("\nLESSON 1")
+
+# OOPS
+# Accidentally removed this file.
+# Need to maintian better GIT control to make sure this doesn't happen.
+# Polices are changed, and best practices are being implemented.
--- PythonTutorial.py
+++ PythonTutorial.py
@@ -1,1 +1,21 @@
-# CH 7 - LOOPS
+# CH 8 - FUNCTIONS
+
+# LESSON 1
+print("\nLESSON 1")
+# definition of functions are defined by the word 'def'
+def myFunction():
+    print("Hello, from myFunction!")
+    
+
+# functions can also contain arguments
+def addTwoNumbers(a, b):
+    return a + b
+
+
+# LESSON 2 - Calling a function
+print("\nLESSON 2")
+def myFunctionWithArgs(usernmae, greeting):
+    print("Hello, %s, From My Function! I wish you %s" % (username, greeting))
+    
+# print a simple greeting
+myFunction()
