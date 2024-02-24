59185f455ea5787af7972bb6b51408175e309b04
Kaleb Soller <kalebsoller@gmail.com>
2024-02-24 15:25:49 -0500

    commit

--- scripts/shopping_cart.py
+++ scripts/shopping_cart.py
@@ -9,17 +9,23 @@ from typing import List
 
 
 class ShoppingCart:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, max_size: int) -> None:
+        self.items_in_cart: List[str] = []
+        self.max_size = max_size
 
     def add(self, item: str):
-        pass
+        if self.size() == self.max_size:
+            raise OverflowError("Cannot add more items\n")
+        self.items_in_cart.append(item)
 
     def size(self) -> int:
-        return 0
+        return len(self.items_in_cart)
 
     def get_items(self) -> List[str]:
-        return []
+        return self.items_in_cart
 
     def get_total_price(self, price_map):
-        pass
+        total_price = 0
+        for lookup_item in self.items_in_cart:
+            total_price += price_map.get(lookup_item)
+        return total_price
--- tests/test_shopping_cart.py
+++ tests/test_shopping_cart.py
@@ -1,10 +1,57 @@
-from scripts import shopping_cart
+from scripts.shopping_cart import ShoppingCart
 import pytest
+from unittest.mock import Mock
+from scripts.item_database import ItemDatabase
 
 
-def pass_function():
+@pytest.fixture
+def cart():
+    # All the setup for the cart here...
+    # This fixture is created new for every test function.
+    # You can put this file in the '/tests/conftest.py' file.
+    return ShoppingCart(5)
+
+
+def test_pass_function():
     pass
 
 
-def can_add_item_to_cart():
-    pass
+def test_can_add_item_to_cart(cart):
+    # cart: ShoppingCart = ShoppingCart(5)  # 'cart' was created as a fixture and passed as an argument,
+    cart.add("apple")
+    assert cart.size() == 1
+
+
+def test_when_item_added_cart_contains_item(cart):
+    # cart: ShoppingCart = ShoppingCart(5)  # 'cart' was created as a fixture and passed as an argument,
+    cart.add("apple")
+    assert "apple" in cart.get_items()
+
+
+def test_when_more_than_max_items(cart):
+    # cart: ShoppingCart = ShoppingCart(5)  # 'cart' was created as a fixture and passed as an argument,
+    for _ in range(5):  # Prefill the cart with 5 items.
+        cart.add("apple")
+
+    with pytest.raises(OverflowError):
+        cart.add("apple")  # Prefill allows only raises on the 6th item.
+
+
+def test_can_get_total_price(cart):
+    # cart: ShoppingCart = ShoppingCart(5)  # 'cart' was created as a fixture and passed as an argument,
+    cart.add("apple")
+    cart.add("orange")
+    item_database = ItemDatabase()
+
+    def mock_get_item(item: str):
+        if item == "apple":
+            return 1.0
+        if item == "orange":
+            return 2.0
+
+    # item_database.get = Mock( price_map={'apple':1.0,'orange':2.0} )
+    # item_database.get = Mock( return_value = 1.0 )
+    item_database.get = Mock(side_effect=mock_get_item)
+    # item_database.get()  # Future implementation.
+
+    assert cart.get_total_price(item_database) == 3.0
